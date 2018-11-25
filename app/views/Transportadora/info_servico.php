ccb8f565f5149593a62f180f878373f01321546f
Bruno Pinheiro <eubrunopinheiro@gmail.com>
2018-11-25 11:59:23 -0200

    Add files via upload

---
+++ app/views/Transportadora/info_servico.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Owl
+ * Date: 24/11/2018
+ * Time: 19:06
+ */
---
+++ app/views/Transportadora/perfilTransportadora.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>The way - A página que guia você</title>
+
+  <link href="../../../assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../../assets/css/animate.min.css" rel="stylesheet">
+  <link href="../../../assets/css/font-awesome.min.css" rel="stylesheet">
+  <link href="../../../assets/css/lightbox.css" rel="stylesheet">
+  <link href="../../../assets/css/main.css" rel="stylesheet">
+  <link id="css-preset" href="../../../assets/css/presets/preset1.css" rel="stylesheet">
+  <link href="../../../assets/css/responsive.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../../../assets/css/estilotw.css">
+  <link rel="shortcut icon" href="../../../assets/images/logo.png">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+  <link href="../../views/Y-Importar/cabecalho_padrao.php">
+  <link href="../../views/Y-Importar/rodape.php">
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <script src="maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="cdnjs.cloudflare.com/ajax/libs/jquery/3.3.7/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.7/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+  <script type="text/javascript" src="../../../assets/js/jquery.js"></script>
+  <script type="text/javascript" src="../../../assets/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../../../assets/js/jquery.inview.min.js"></script>
+  <script type="text/javascript" src="../../../assets/js/wow.min.js"></script>
+  <script type="text/javascript" src="../../../assets/js/mousescroll.js"></script>
+  <script type="text/javascript" src="../../../assets/js/smoothscroll.js"></script>
+  <script type="text/javascript" src="../../../assets/js/jquery.countTo.js"></script>
+  <script type="text/javascript" src="../../../assets/js/lightbox.min.js"></script>
+  <script type="text/javascript" src="../../../assets/js/main.js"></script>
+  <script src="../../../assets/js/html5shiv.js"></script>
+  <script src="../../../assets/js/respond.min.js"></script>
+
+</head><!--/head(Cabeça)-->
+<body>
+
+<div class="preloader"> <i class="fa fa-circle-o-notch fa-spin"></i></div>
+
+<header id=" Home ">
+  <div class="main-nav">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only"> Alternar de navegação </span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="../../../index.html">
+          <h1><img class="img-responsive" src="../../../assets/images/T.png" alt="logo"></h1>
+        </a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+
+          <li class="scroll"><a href="../Y-Importar/ajuda.html">       Ajuda        </a></li>
+          <li class="scroll"><a href="../../../index.html"> Sair         </a></li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+</header>
+
+<div class="container bootstrap snippet">
+  <div class="row">
+    <div class="col-sm-10"><h1> Petobras </h1></div>
+    <div class="col-sm-2"><a href="#users" class="pull-right"></a></div>
+  </div>
+  <div class="row">
+    <div class="col-sm-3">
+
+
+      <div class="text-center">
+        <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar">
+      </div>
+
+
+      <div class="panel panel-default">
+        <div class="panel-heading">Seu Site <i class="fa fa-link fa-1x"></i></div>
+        <div class="panel-body"><a href="http://www.petrobras.com.br/pt/">petrobras.com</a></div>
+      </div>
+
+
+      <ul class="list-group">
+        <li class="list-group-item text-muted">Atividade <i class="fa fa-dashboard fa-1x"></i></li>
+        <li class="list-group-item text-right"><span class="pull-left"><strong>Serviços cadastrados</strong></span> 1525</li>
+        <li class="list-group-item text-right"><span class="pull-left"><strong>Serviços em andamento</strong></span> 13</li>
+      </ul>
+
+
+    </div><!--/col-3-->
+    <div class="col-sm-9">
+      <ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+        <li><a data-toggle="tab" href="#messages">Cadastrar Serviço</a></li>
+      </ul>
+
+
+      <div class="tab-content">
+        <div class="tab-pane active" id="home">
+          <hr>
+          <form class="form" action="#" method="post" id="hometrasportadora">
+            <h1>Ola Pétrobras</h1>
+
+            <hr>
+          </form>
+        </div><!--/tab-pane-->
+        <div class="tab-pane" id="messages">
+
+          <h2></h2>
+
+          <hr>
+          <form class="form" action="#" method="post" id="regitraserviço">
+            <div class="form-group">
+
+              <div class="col-xs-6">
+                <label for="nomecarga"><h4>Nome da garga</h4></label>
+                <input type="text" class="form-control" name="nomecarga" id="nomecarga" placeholder="Nome da carga">
+              </div>
+            </div>
+            <div class="form-group">
+
+              <div class="col-xs-6">
+                <label for="tipocarga"><h4>Tipo de carga</h4></label>
+                <input type="text" class="form-control" name="tipocarga" id="tipocarga" placeholder="tipo de carga">
+              </div>
+            </div>
+
+            <div class="form-group">
+
+              <div class="col-xs-6">
+                <label for="localsaida"><h4>Local de saída</h4></label>
+                <input type="text" class="form-control" name="localsaida" id="localsaida" placeholder="Local de saída" >
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-xs-6">
+                <label for="localentrega"><h4>Local de entrega</h4></label>
+                <input type="text" class="form-control" name="localentrega" id="localentrega" placeholder="Local de entrega">
+              </div>
+            </div>
+            <div class="form-group">
+
+              <div class="col-xs-6">
+                <label for="contatoserviço"><h4>Contato do serviço</h4></label>
+                <input type="email" class="form-control" name="contatoserviço" id="contatoserviço" placeholder="contato do serviço" >
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-xs-12">
+                <br>
+                <button class="btn btn-lg btn-success" type="submit"><i class="glyphicon glyphicon-ok-sign"></i>Cadastrar</button>
+              </div>
+            </div>
+          </form>
+
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<footer id="footer">
+  <div class="footer-top wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+    <div class="container text-center">
+      <div class="footer-logo">
+        <a href="../../../index.html"><img class="img-responsive" src="../../../assets/images/logo.png" alt=""></a>
+      </div>
+      <div class="social-icons">
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <p> &copy; 2018 The way. </p>
+        </div>
+        <div class="col-sm-6">
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</body>
+</html>
